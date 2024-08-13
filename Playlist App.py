3c5c0039a4e1c5003fca65874863520d9904d336
Marko <markogidej@yahoo.com>
2024-08-13 14:47:42 -0400

    Init Commit

---
+++ Playlist App.py
@@ -0,0 +1,33 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Task 1: Data Organization with Lists and Dictionaries:
+
+
+
+
+# Task 2: Efficient Search and Sort Algorithms for Playlist Navigation:
+
+
+
+# Task 3: Building a Flask API for Playlist Access and Management:
+## Song Engpoints
+# Create Song
+# Update Song
+# Delete Song
+# Seach/Get Song
+
+## Playlist Endpoints
+# Create Playlist
+# Get Playlist
+# Update Playlist
+# Delete Playlist
+
+## Additional Endpoints:
+# Add song to Playlist
+# Remove song from Playlist
+# Sort songs in Playlist by song name, genre, and artist
+
+if __name__ == '__main__':
+    app.run(debug=True)
