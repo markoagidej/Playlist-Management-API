2ef53c67194a2415f7210392ea38e355443a2c37
Marko <markogidej@yahoo.com>
2024-08-15 18:02:39 -0400

    Testing done

--- Playlist App.py
+++ Playlist App.py
@@ -19,41 +19,51 @@ def create_song(artist, album, title, genre):
 
 def update_song(artist, album, title, genre):
     song_to_update = get_song(title)
-    delete_song(song_to_update)
     new_song = create_song(artist, album, title, genre)
-    for playlist in playlist_dictionary.values():
-        for song in playlist:
+    for playlist, songList in playlist_dictionary.items():
+        for song in songList:
             if song == song_to_update:
-                song = new_song
+                playlist_dictionary[playlist].append(new_song)
+    delete_song(song = song_to_update)
 
-def delete_song(song):
-    song_list.remove(song)
-    for playlist in playlist_dictionary.values():
-        while song in playlist:
-            playlist.remove(song)
+def delete_song(**kwargs):
+    song = kwargs.get("song", None)
+    title = kwargs.get("title", None)
+    if song:
+        if song in song_list:
+            song_list.remove(song)
+            for playlist in playlist_dictionary.values():
+                while song in playlist:
+                    playlist.remove(song)
+    elif title:
+        song = get_song(title)
+        delete_song(song = song)
 
 def get_song(title):
     for song in song_list:
         if song['title'] == title:
             return song
+        else:
+            print(f"No song found with title of: {title}")
 
 # Playlists
 def create_playlist(name):
-    if not playlist_dictionary[name]:
-        playlist_dictionary[name] = []
-    else:
+    if name in playlist_dictionary:
         print(f"Playlist titled '{name}' already exists!")
+    else:
+        playlist_dictionary[name] = []
 
-def update_playlist(name):
-    playlist_to_update, song_list = get_playlist(name)
-    playlist_dictionary[name] = song_list
+def update_playlist(old_name, new_name):
+    playlist_to_update, song_list = get_playlist(old_name)
+    playlist_dictionary[new_name] = song_list
     delete_playlist(playlist_to_update)
 
 def get_playlist(name):
     return name, playlist_dictionary[name]
 
 def delete_playlist(name):
-    del playlist_dictionary[name]
+    if name in playlist_dictionary:
+        del playlist_dictionary[name]
 
 # Song/Playlist interactions
 def add_song_to_playlist(song_title, playlist_name):
@@ -69,15 +79,15 @@ def remove_song_from_playlist(song_title, playlist_name):
 
 # Task 2: Efficient Search and Sort Algorithms for Playlist Navigation:
 
-def sort_playlist(playlist_name, sort_category):
-    song_list = playlist_dictionary[playlist_name]
+def sort_playlist(song_list, sort_category):
+    # song_list = playlist_dictionary[playlist_name]
     if len(song_list) > 1:
         mid = len(song_list) // 2
         left = song_list[:mid]
         right = song_list[mid:]
 
-        sort_playlist(left)
-        sort_playlist(right)
+        sort_playlist(left, sort_category)
+        sort_playlist(right, sort_category)
 
         left_index = right_index = main_index = 0
 
@@ -117,7 +127,7 @@ def updateSong():
 @app.route('/songs', methods=['DELETE'])
 def deleteSong():
     data = request.get_json()
-    delete_song(create_song(data['artist'], data['album'], data['title'], data['genre']))
+    delete_song(song = create_song(data['artist'], data['album'], data['title'], data['genre']))
 # Seach/Get Song
 @app.route('/songs', methods=['GET'])
 def getSong():
@@ -136,10 +146,10 @@ def getPlaylist():
     data = request.get_json()
     return jsonify(get_playlist(data['name']))
 # Update Playlist
-@app.route('/playlists', methods=['PUT'])
-def updatePlaylist():
+@app.route('/playlists/<string:playlist_name>', methods=['PUT'])
+def updatePlaylist(playlist_name):
     data = request.get_json()
-    update_playlist(data['name'])
+    update_playlist(playlist_name, data['name'])
 # Delete Playlist
 @app.route('/playlists', methods=['DELETE'])
 def deletePlaylist():
@@ -160,10 +170,124 @@ def removeFromPlaylist(playlist_name):
     song_title = (data['song_title'])
     remove_song_from_playlist(song_title, playlist_name)
 # Sort songs in Playlist by song name, genre, and artist
-@app.route('/playlists/<string:playlist_name>', methods=['PUT'])
-def sortPlaylist(playlist_name):
+@app.route('/playlists/sort/<string:playlist_name>', methods=['PUT'])
+def sortSelectedPlaylist(playlist_name):
     data = request.get_json()
     sort_playlist(playlist_name, data['sort_category'])
+
+# Testing
+# Creating songs
+create_song("Artist 1", "Album 1", "A1A1T1", "Genre 1")
+create_song("Artist 1", "Album 1", "A1A1T2", "Genre 1")
+create_song("Artist 1", "Album 1", "A1A1T3", "Genre 1")
+
+create_song("Artist 2", "Album 1", "A2A1T1", "Genre 1")
+create_song("Artist 2", "Album 1", "A2A1T2", "Genre 1")
+create_song("Artist 2", "Album 1", "A2A1T3", "Genre 1")
+
+create_song("Artist 2", "Album 2", "A2A2T1", "Genre 2")
+create_song("Artist 2", "Album 2", "A2A2T2", "Genre 2")
+create_song("Artist 2", "Album 2", "A2A2T3", "Genre 2")
+
+create_song("Artist 3", "Album 1", "A3A1T1", "Genre 3")
+create_song("Artist 3", "Album 1", "A3A1T2", "Genre 4")
+create_song("Artist 3", "Album 1", "A3A1T3", "Genre 2")
+
+# Printing song list
+print("All songs in song_list:")
+for song in song_list:
+    print(song)
+
+# Printing result of get_song()
+print("\nPrinting certain song:")
+print(get_song('A2A2T2'))
+
+# Creating playists
+create_playlist("All of Artist 2")
+create_playlist("All of Genre 2")
+# Testing creation of existing playlist
+create_playlist("All of Artist 2")
+# Printing empty playlists
+print("Empty playlists that exist:")
+for playlist in playlist_dictionary:
+    print(playlist)
+
+# Populating playlists
+add_song_to_playlist("A2A1T1", "All of Artist 2")
+add_song_to_playlist("A2A1T2", "All of Artist 2")
+add_song_to_playlist("A2A1T3", "All of Artist 2")
+add_song_to_playlist("A2A2T1", "All of Artist 2")
+add_song_to_playlist("A2A2T2", "All of Artist 2")
+add_song_to_playlist("A2A2T3", "All of Artist 2")
+
+add_song_to_playlist("A2A2T1", "All of Genre 2")
+add_song_to_playlist("A2A2T2", "All of Genre 2")
+add_song_to_playlist("A2A2T3", "All of Genre 2")
+add_song_to_playlist("A3A1T3", "All of Genre 2")
+
+# Testing song updating with songs in playlists
+update_song("Update artist", "Update Album", "A2A2T2", "Update Genre")
+# Printing song list
+print("All songs in song_list after updated song:")
+for song in song_list:
+    print(song)
+# Print playlists with updated song
+print("Print playlists with updated song:")
+for playlist, songs in playlist_dictionary.items():
+    print(playlist)
+    for song in songs:
+        print(song)
+
+# Deleting song
+delete_song(title = "A2A2T3")
+# Printing song list
+print("All songs in song_list after deleting song:")
+for song in song_list:
+    print(song)
+# Print playlists after deleting song
+print("Print playlists with deleted song:")
+for playlist, songs in playlist_dictionary.items():
+    print(playlist)
+    for song in songs:
+        print(song)
+
+# Updating a playlist name
+update_playlist("All of Genre 2", "Genre 2 and Updated")
+# Printing playlists after playlist update
+print("Print playlists after updating playlist name:")
+print(playlist_dictionary.keys())
+
+# Removing song from playlist
+remove_song_from_playlist("A2A2T2", "All of Artist 2")
+# Printing playlist after deleting song from playlist
+print("Printing songs in playlist after deleting song from playlist")
+for song in playlist_dictionary["All of Artist 2"]:
+    print(song)
+
+# Playlist sorting test
+create_playlist("Sorting Playlist")
+add_song_to_playlist("A3A1T3", "Sorting Playlist")
+add_song_to_playlist("A1A1T1", "Sorting Playlist")
+add_song_to_playlist("A3A1T1", "Sorting Playlist")
+add_song_to_playlist("A2A2T1", "Sorting Playlist")
+
+# Sorting by each sort option (title, album, genre)
+# title
+sort_playlist(playlist_dictionary["Sorting Playlist"], 'title')
+print("Printing songs in Sorting Playlist after sorting by title:")
+for song in playlist_dictionary["Sorting Playlist"]:
+    print(song)
+# genre
+sort_playlist(playlist_dictionary["Sorting Playlist"], 'genre')
+print("Printing songs in Sorting Playlist after sorting by genre:")
+for song in playlist_dictionary["Sorting Playlist"]:
+    print(song)
+# album
+sort_playlist(playlist_dictionary["Sorting Playlist"], 'album')
+print("Printing songs in Sorting Playlist after sorting by album:")
+for song in playlist_dictionary["Sorting Playlist"]:
+    print(song)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
